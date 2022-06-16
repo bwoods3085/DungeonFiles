b67d58062428a1c4ab2a257bdd02f8ce927cf576
bwoods3085 <blake.rwoods@outlook.com>
2022-06-15 20:37:38 -0500

    Created Character Class and menu in Program.cs

--- Dungeon/Dungeon/Program.cs
+++ Dungeon/Dungeon/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DungeonLibrary;
+
+namespace Dungeon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Next Generation";
+            Console.WriteLine("Space...The Final Frontier..\n");
+
+            int score = 0;
+
+            bool exit = false;
+            do
+            {
+
+                bool reload = false;
+                do
+                {
+                    Console.WriteLine("\nChoose an Action:\n" +
+                        "A) Attack\n" +
+                        "W) Warp Speed Away\n" +
+                        "P) Player Info\n" +
+                        "E) Enemy Info\n" +
+                        "X) Exit\n");
+
+                    string userOption = Console.ReadKey(true).Key.ToString().ToUpper();
+                    Console.Clear();
+
+                    switch (userOption)
+                    {
+                        case "A":
+                            
+                            break;
+                        case "W":
+                            break;
+                        case "P":
+                            break;
+                        case "E":
+                            break;
+                        case "X":
+                            Console.WriteLine("You Did Not Go Boldly...");
+                            reload = true;
+                            break;
+
+                        default:
+                            break;
+                    }//End switch
+
+                } while (!reload && !exit);
+
+            } while (!exit);
+
+        }//End Main().
+
+        private static string GetRoom()
+        {
+            string[] rooms = new string[]
+            {
+
+            };
+
+            Random rand = new Random();
+
+            int indexNbr = rand.Next(rooms.Length);
+
+            string room = rooms[indexNbr];
+            return room;
+
+        }//End GetRoom()
+    }//End class
+}//End namespace
---
+++ Dungeon/DungeonLibrary/Character.cs
@@ -0,0 +1,73 @@
+﻿namespace DungeonLibrary
+{
+    public class Character
+    {
+        //Fields
+        private int _life;
+        private int _maxLife;
+        private string _name;
+        private int _hitChance;
+        private int _block;
+
+        //Properties
+
+        public int MaxLife
+        {
+            get { return _maxLife; }
+            set { _maxLife = value; }
+        }
+        public int Life
+        {
+            get { return _life; }
+            set
+            {
+                if (value <= MaxLife)
+                {
+                    _life = value;
+                }
+                else
+                {
+                    _life = MaxLife;
+                }//End Else
+
+            }//End set
+        }//End prop
+
+        public string Name
+        {
+            get { return _name; }  
+            set { _name = value; }
+        }
+        public int HitChance
+        {
+            get { return _hitChance; }
+            set { _hitChance = value; }
+        }
+        public int Block
+        {
+            get { return _block; }
+            set { _block = value; }
+        }
+
+        //Ctors
+        public Character(int life, int maxLife, string name, int hitChance, int block)
+        {
+            Life = life;
+            MaxLife = maxLife;
+            Name = name;
+            HitChance = hitChance;
+            Block = block;
+        }
+        public override string ToString()
+        {
+            return string.Format("<======<({0})>======>\n" +
+                "Life: {1} of {2}\nHit Chance: {3}%\n" +
+                "Block: {4}",Name,Life,MaxLife,HitChance,Block);
+            
+        }
+
+
+
+
+    }//End Class
+}//End namespace
